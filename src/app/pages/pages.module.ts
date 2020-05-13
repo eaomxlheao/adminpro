e964ebc4a7f36941cece9adcfab9374af3399aae
EduArdO <eaomxlheao@gmail.com>
2020-05-13 17:49:32 -0500

    Agregando Graficas con ng2-charts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { ChartsModule } from "ng2-charts";
 
 import { PAGES_ROUTES } from "./pages.routes";
 
@@ -11,6 +12,7 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
+import { GraficoDonaComponent } from "../componets/grafico-dona/grafico-dona.component";
 
 @NgModule({
   declarations: [
@@ -19,8 +21,9 @@ import { IncrementadorComponent } from "../components/incrementador/incrementado
     ProgressComponent,
     Graficas1Component,
     IncrementadorComponent,
+    GraficoDonaComponent,
   ],
   exports: [DashboardComponent, ProgressComponent, Graficas1Component],
-  imports: [SharedModule, PAGES_ROUTES, FormsModule],
+  imports: [SharedModule, PAGES_ROUTES, FormsModule, ChartsModule],
 })
 export class PagesModule {}
