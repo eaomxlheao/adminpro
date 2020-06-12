043d366ec64fe982a207a9e62eabe31088c1f123
EduArdO <eaomxlheao@gmail.com>
2020-06-11 19:57:45 -0500

    Validaciones en ReactiveForms Validators

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -15,16 +15,49 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     init_plugins();
-    this.forma = new FormGroup({
-      nombre: new FormControl(null, Validators.required),
-      correo: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, Validators.required),
-      password2: new FormControl(null, Validators.required),
-      condiciones: new FormControl(false),
+    this.forma = new FormGroup(
+      {
+        nombre: new FormControl(null, Validators.required),
+        correo: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, Validators.required),
+        password2: new FormControl(null, Validators.required),
+        condiciones: new FormControl(false),
+      },
+      { validators: this.sonIguales("password", "password2") }
+    );
+
+    this.forma.setValue({
+      nombre: "EduArdO",
+      correo: "eaomxlheao@gmail.com",
+      password: "moquito1",
+      password2: "moquito2",
+      condiciones: true,
     });
   }
 
   registrarUsuario() {
+    if (this.forma.invalid) {
+      return;
+    }
+
+    if (this.forma.value.condiciones) {
+      console.log("Debe aceptar las condiciones");
+    }
+
     console.log(this.forma.value);
+  }
+
+  sonIguales(campo1: string, campo2: string) {
+    return (group: FormGroup) => {
+      let val1 = group.controls[campo1].value;
+      let val2 = group.controls[campo2].value;
+
+      if (val1 === val2) {
+        return null;
+      }
+      return {
+        sonIguales: true,
+      };
+    };
   }
 }
