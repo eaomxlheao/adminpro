56ebf98bf1d71e19b67880c49d6a7ad46b929e08
EduArdO <eaomxlheao@gmail.com>
2020-06-14 20:44:05 -0500

    Google token Auth2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,8 +4,10 @@ import { NgForm } from "@angular/forms";
 import { Usuario } from "../models/usuario.model";
 import { HttpClient } from "@angular/common/http";
 import { UsuarioService } from "../services/service.index";
+import { element } from "protractor";
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: "app-login",
@@ -15,14 +17,38 @@ declare function init_plugins();
 export class LoginComponent implements OnInit {
   public email: string;
   public recuerdame: boolean = false;
+  public auth2; // The Sign-In object.
+
   constructor(public router: Router, public usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
     init_plugins();
+    this.googleInit();
     this.email = localStorage.getItem("email") || "";
     if (this.email.length > 0) {
       this.recuerdame = true;
     }
+  }
+
+  googleInit() {
+    gapi.load("auth2", () => {
+      this.auth2 = gapi.auth2.init({
+        client_id:
+          "731389811270-1o1en3guv7q2n80cjrrsi2kuj0a8fs1o.apps.googleusercontent.com",
+        coockiepolicy: "single_host_origin",
+        scope: "profile email",
+      });
+
+      this.attachSignIn(document.getElementById("btnGoogle"));
+    });
+  }
+
+  attachSignIn(element) {
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      //let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+      console.log(token);
+    });
   }
 
   ingresar(forma: NgForm) {
